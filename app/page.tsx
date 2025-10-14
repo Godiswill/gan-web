8c4fe660e1bc1350c81de17a36164338be88c00c
godis <isunrj@icloud.com>
2025-10-14 12:00:27 +0800

    feat: tmp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
-import MDIndex from '@/content/index.md';
-import SelectImage from '@/components/SelectImage';
+import AIEdit from '@/components/AIEdit';
 
-const pageTitle = 'AI Background Remover';
-const secondTitle = 'Free, Batch, No Login, No Quality Loss';
 const title =
   'BgGone - Free AI Background Remover. Free, Batch, No Login, No Quality Loss';
 const description =
@@ -15,26 +12,5 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-center py-6 sm:py-8 text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">
-        {pageTitle}
-        <p className="mt-2 sm:mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">
-          {secondTitle}
-        </p>
-      </h1>
-      <SelectImage />
-      <div className="mt-5 md:mt-8 px-4 main-width">
-        <div className="prose dark:prose-invert max-w-max">
-          <MDIndex />
-        </div>
-
-        <div className="mt-4">
-          <a className="cursor-pointer hover:underline" id="back-to-top">
-            Remove Backgrounds Now
-          </a>
-        </div>
-      </div>
-    </>
-  );
+  return <AIEdit />;
 }
